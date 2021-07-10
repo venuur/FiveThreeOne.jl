12e941e81605d93f14d9b2f3a7744c8be2b68b94
Carl Morris <carl.morris.world@gmail.com>
2021-07-09 17:14:09 -0700

    Add anchor week for upcoming cycle

--- scratch.jl
+++ scratch.jl
@@ -35,3 +35,26 @@ function write_deload_week()
         end
     end
 end
+
+function anchor_week(week)
+    sets = [
+        vcat(F.warmup_sets(SQUAT), F.main_lifts(SQUAT, week, F.Order531, true), F.widowmaker(SQUAT, week, F.Order531)),
+        vcat(F.warmup_sets(BENCH), F.main_lifts(BENCH, week, F.Order531, true), F.widowmaker(BENCH, week, F.Order531)),
+        vcat(F.warmup_sets(DEADLIFT), F.main_lifts(DEADLIFT, week, F.Order531, true), F.widowmaker(DEADLIFT, week, F.Order531)),
+        vcat(F.warmup_sets(PRESS), F.main_lifts(PRESS, week, F.Order531, true), F.widowmaker(PRESS, week, F.Order531)),
+    ]
+    F.print_main_lift_table(["Squat", "Bench Press", "Deadlift", "Press"], sets)
+    daily_assistance = [
+        F.AssistanceLift("KB Swing", 35, 10, 10),
+        F.AssistanceLift("Push-Up", 0, 5, 10),
+        F.AssistanceLift("Chin-Up", 0, 5, 4),
+        F.AssistanceLift("Hanging Knee Raises", 0, 5, 10),
+    ]
+    assistance_sets = [
+        daily_assistance,
+        daily_assistance,
+        daily_assistance,
+        daily_assistance,
+    ]
+    F.print_assistance_lift_table(assistance_sets)
+end
