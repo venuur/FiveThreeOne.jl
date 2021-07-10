3dd8a59aefe66ca7d147acf03bfa4c1c99c44487
Carl Morris <carl.morris.world@gmail.com>
2021-07-10 07:21:44 -0700

    Add PR set goals for main lifts with PR sets

--- src/FiveThreeOne.jl
+++ src/FiveThreeOne.jl
@@ -31,10 +31,15 @@ struct MainLift
     weight::Float64
     sets::Int
     reps::Reps
+    e1rm_to_beat::Union{Nothing, Float64}
 end
 
-MainLift(percentage, weight, sets, reps::Int) = MainLift(percentage, weight, sets, Reps(reps))
-MainLift(percentage, weight, sets, reps::UnitRange{Int}) = MainLift(percentage, weight, sets, Reps(reps))
+MainLift(percentage, weight, sets, reps::Int) = MainLift(percentage, weight, sets, Reps(reps), nothing)
+MainLift(percentage, weight, sets, reps::UnitRange{Int}) = MainLift(percentage, weight, sets, Reps(reps), nothing)
+MainLift(percentage, weight, sets, reps::Reps) = MainLift(percentage, weight, sets, reps, nothing)
+MainLift(percentage, weight, sets, reps::Int, e1rm_to_beat) = MainLift(percentage, weight, sets, Reps(reps), e1rm_to_beat)
+MainLift(percentage, weight, sets, reps::UnitRange{Int}, e1rm_to_beat) = MainLift(percentage, weight, sets, Reps(reps), e1rm_to_beat)
+
 
 struct AssistanceLift
     name::AbstractString
@@ -75,12 +80,26 @@ function print_main_lift_table(names, lifts::Vector{Vector{MainLift}})
     n_longest_lift = maximum([length(lift) for lift in lifts])
     row = 1
     while row <= n_longest_lift
+        has_pr_set = false
         for lift in lifts
             if row <= length(lift)
                 entry = lift[row]
+                has_pr_set = has_pr_set || entry.reps.is_pr_set
                 @printf "%4.0f %10.1f %6d %6s |" entry.percentage entry.weight entry.sets entry.reps
             else
                 print(repeat(" ", 26))
+            end
+        end
+        if has_pr_set
+            println()
+            for lift in lifts
+                if row <= length(lift)
+                    entry = lift[row]
+                    pr_goal = pr_set_goal_reps(entry.weight, entry.e1rm_to_beat)
+                    @printf "  %6s reps for E1RM %6.1f |" pr_goal.reps pr_goal.new_e1rm
+                else
+                    print(repeat(" ", 26))
+                end
             end
         end
         print("\n")
@@ -128,6 +147,9 @@ function print_assistance_lift_table(lifts::Vector{Vector{AssistanceLift}})
 end
 
 make_single_sets(percentages, weights, reps) = collect(map(MainLift, percentages, weights, repeat([1], length(percentages)), reps))
+function make_single_sets(percentages, weights, reps, e1rm_to_beat)
+    collect(map(MainLift, percentages, weights, repeat([1], length(percentages)), reps, repeat([e1rm_to_beat], length(percentages))))
+end
 round_to_half_pound(x) = round(x * 2, RoundNearest) / 2
 make_weights(percentages, training_max) = round_to_half_pound.(percentages / 100 .* training_max)
 
@@ -139,7 +161,7 @@ function warmup_sets(training_max)
     return make_single_sets(percentages, weights, reps)
 end
 
-function main_lifts(training_max, week, order, pr_sets)
+function main_lifts(training_max, week, order, pr_sets, e1rm_to_beat)
     make_pr_reps(reps) = Reps(reps, pr_sets)
 
     if order === Order351 && week === Week1
@@ -160,7 +182,7 @@ function main_lifts(training_max, week, order, pr_sets)
         throw(DomainError("Argument `week` must be Week1, Week2, or Week3."))
     end
     weights = make_weights(percentages, training_max)
-    return make_single_sets(percentages, weights, reps)
+    return make_single_sets(percentages, weights, reps, e1rm_to_beat)
 end
 
 function deload_lifts(training_max)
@@ -195,5 +217,17 @@ end
 e1rm(weight, reps::Int) = weight * reps * 0.0333 + weight
 e1rm(weight, reps::UnitRange{Int}) = collect(map(x -> e1rm(weight, x), reps))
 e1rm(weight, reps::Reps) = e1rm(weight, reps.number)
+
+struct PRGoal
+    reps::Reps
+    new_e1rm::Float64
+end
+
+function pr_set_goal_reps(pr_set_weight, e1rm_to_beat)
+    rep_range = 1:50
+    e1rm_above_goal = e1rm(pr_set_weight, rep_range) .>= e1rm_to_beat
+    goal_reps = collect(rep_range)[e1rm_above_goal][1]
+    return PRGoal(Reps(goal_reps), e1rm(pr_set_weight, goal_reps))
+end
 
 end
